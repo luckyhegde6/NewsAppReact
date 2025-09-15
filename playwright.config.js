95fd32dca56058a7094611d26ea714b22871d61f
luckyhegde6 <60867180+luckyhegde6@users.noreply.github.com>
2025-09-16 04:31:29 +0530

    updating the test path

--- playwright.config.js
+++ playwright.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 1 : undefined,
-  reporter: 'html',
+  reporter: [['html', { outputFolder: 'test-results/playwright-report' }]],
   use: {
     baseURL: 'http://localhost:3000',
     trace: 'on-first-retry',
